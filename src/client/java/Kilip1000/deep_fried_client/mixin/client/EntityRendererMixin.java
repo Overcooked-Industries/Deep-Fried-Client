3f660dbd95a157f1552e1916b9c953cd2791461b
Kilip1000 <kilian.lennard.pro@gmail.com>
2026-01-17 16:48:42 +0100

    fix

--- src/client/java/Kilip1000/deep_fried_client/mixin/client/EntityRendererMixin.java
+++ src/client/java/Kilip1000/deep_fried_client/mixin/client/EntityRendererMixin.java
@@ -1,5 +1,6 @@
 package Kilip1000.deep_fried_client.mixin.client;
 
+import Kilip1000.deep_fried_client.ActiveHacks;
 import Kilip1000.deep_fried_client.DeepFriedClientClient;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.world.entity.Entity;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class EntityRendererMixin {
     @Redirect(method = "extractRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isInvisible()Z"))
     boolean alwaysVisiblePlayer(Entity instance){
-        if(instance instanceof Player) return !DeepFriedClientClient.hack_invisibility_bypass && instance.isInvisible();
+        if(instance instanceof Player) return !ActiveHacks.hack_invisibility_bypass && instance.isInvisible();
         return instance.isInvisible();
     }
 }

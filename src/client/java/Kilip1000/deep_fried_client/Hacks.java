fc7f2f83bd8fd62c49976b82d0f51b07fce30e8e
Kilip1000 <kilian.lennard.pro@gmail.com>
2026-01-18 11:12:14 +0100

    chore: cleanup

--- src/client/java/Kilip1000/deep_fried_client/Hacks.java
+++ src/client/java/Kilip1000/deep_fried_client/Hacks.java
@@ -12,25 +12,40 @@ public class Hacks {
 
     public static int hack_cooldown = 0;
 
-    public static void toggleHack(Hack hack){
-        if (hack_cooldown != 0) {hack_cooldown = 2; return;}
-        hack_cooldown = 2;
-        switch (hack) {
-            case FLY -> hack_fly = !hack_fly;
-            case NO_GRAVITY -> hack_no_gravity = !hack_no_gravity;
-            case INVISIBILITY_BYPASS -> hack_invisibility_bypass = !hack_invisibility_bypass;
-            case NO_FALL -> hack_no_fall = !hack_no_fall;
+    public static void toggleHack(Hack hack, boolean show_message){
+        if (hack_cooldown != 0) {
+            hack_cooldown = 2;
+            return;
         }
+        hack_cooldown = 2;
+
         boolean state = false;
         switch (hack) {
-            case FLY -> state = hack_fly;
-            case NO_GRAVITY -> state = hack_no_gravity;
-            case INVISIBILITY_BYPASS -> state = hack_invisibility_bypass;
-            case NO_FALL -> state = hack_no_fall;
+            case FLY -> {
+                hack_fly = !hack_fly;
+                state = hack_fly;
+            }
+            case NO_GRAVITY -> {
+                hack_no_gravity = !hack_no_gravity;
+                state = hack_no_gravity;
+            }
+            case INVISIBILITY_BYPASS -> {
+                hack_invisibility_bypass = !hack_invisibility_bypass;
+                state = hack_invisibility_bypass;
+            }
+            case NO_FALL -> {
+                hack_no_fall = !hack_no_fall;
+                state = hack_no_fall;
+            }
         }
 
         assert Minecraft.getInstance().player != null;
-        Minecraft.getInstance().player.displayClientMessage(Component.literal("§l" + hack.name + "§r§7 was toggled to " + MainHackScreen.colored_bool_text(state) + "§r§7!"), false);
+
+        if(show_message) Minecraft.getInstance().player.displayClientMessage(Component.literal("§l" + hack.name + "§r§7 was toggled to " + MainHackScreen.colored_bool_text(state) + "§r§7!"), false);
+    }
+
+    public static void toggleHack(Hack hack){
+        toggleHack(hack, true);
     }
 
     public enum Hack{

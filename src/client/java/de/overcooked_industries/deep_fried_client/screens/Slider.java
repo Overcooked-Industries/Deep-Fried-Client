b50ac3945aaef5724f8b2069db348e9778c0114e
Kilip1000 <kilian.lennard.pro@gmail.com>
2026-01-24 12:20:16 +0100

    Improve slider code

--- src/client/java/de/overcooked_industries/deep_fried_client/screens/Slider.java
+++ src/client/java/de/overcooked_industries/deep_fried_client/screens/Slider.java
@@ -4,17 +4,17 @@ import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.network.chat.Component;
 
 public class Slider extends AbstractSliderButton {
-    String text = "";
+    String text;
 
-    public Slider(int x, int y, int width, int height, String message, double value) {
-        super(x, y, width, height, Component.literal(message), value);
-        String text = message;
+    public Slider(int x, int y, int width, int height, String text, double value) {
+        super(x, y, width, height, Component.literal(""), value);
+        this.text = text;
         updateMessage();
     }
 
     @Override
     protected void updateMessage() {
-        this.setMessage(Component.literal(text + ": " + String.format("%.2f", this.value)));
+        this.setMessage(Component.literal(String.format("%s: %.2f", text, this.value)));
     }
 
     @Override
